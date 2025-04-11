4b338f6090be8070989d7baf9771da21f00087a8
marlon <marlonadilson@gmail.com>
2025-04-11 16:23:29 -0300

    feat: initial configuration

---
+++ src/test/java/com/erpcom/erpcom/ErpcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erpcom.erpcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
