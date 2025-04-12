c3cda2adc94ac60969beebb9bca63dc3bfd698e7
marlon <marlonadilson@gmail.com>
2025-04-12 13:50:54 -0300

    refactor: usando anotações do Lombok para  criação de getter setter e construtores

--- src/main/java/com/erpcom/erpcom/dtos/ClienteResponseDTO.java
+++ src/main/java/com/erpcom/erpcom/dtos/ClienteResponseDTO.java
@@ -1,10 +1,14 @@
 package com.erpcom.erpcom.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClienteResponseDTO {
     private String cgc;
     private String razaoSocial;
@@ -18,20 +22,4 @@ public class ClienteResponseDTO {
     private String fone;
     private String fax;
     private String celular;
-
-    public ClienteResponseDTO(String cgc, String razaoSocial, String nomeFantasia, String inscEstadual,String endereco, String bairro, String cidade, String uf, String cep,String fone, String fax, String celular) {
-        this.cgc = cgc;
-        this.razaoSocial = razaoSocial;
-        this.nomeFantasia = nomeFantasia;
-        this.inscEstadual = inscEstadual;
-        this.endereco = endereco;
-        this.bairro = bairro;
-        this.cidade = cidade;
-        this.uf = uf;
-        this.cep = cep;
-        this.fone = fone;
-        this.fax = fax;
-        this.celular = celular;
-    }
-
 }
