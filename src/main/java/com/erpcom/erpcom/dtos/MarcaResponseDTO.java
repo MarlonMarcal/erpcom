211b21d80959f5207b003c324277e4775f085ade
marlon <marlonadilson@gmail.com>
2025-04-12 17:38:47 -0300

    feat:  craicao DTO marca

---
+++ src/main/java/com/erpcom/erpcom/dtos/MarcaResponseDTO.java
@@ -0,0 +1,17 @@
+package com.erpcom.erpcom.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MarcaResponseDTO {
+
+    private Integer codMarca;
+    private String marca;
+
+}
